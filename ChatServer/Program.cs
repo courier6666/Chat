a0fce08e9f3b9e436306da5546283ccfa5ebef79
Yuriy <petzik.popovich@gmail.com>
2025-09-04 20:10:14 +0300

    configured pipeline for tcp server

--- ChatServer/Program.cs
+++ ChatServer/Program.cs
@@ -8,6 +8,11 @@ using ChatServer;
 using ChatServer.Extensions;
 using Microsoft.Data.Sqlite;
 using TcpServer.Core.Builder;
+using Chat.Message;
+using System.IO;
+using Chat.DatabaseAccess;
+using TcpServer.Core.Pipeline.Components;
+using TcpServer.Core.MessageComponents;
 
 using var conn = new SqliteConnection("Data Source=Chat.db");
 conn.Open();
@@ -20,22 +25,33 @@ command.CommandText = """
                         TimeUtc DATETIME NOT NULL);
 
                       """;
-command.ExecuteNonQuery();
+await command.ExecuteNonQueryAsync();
 
 DapperExtensions.DapperStartup();
 
 var builder = TcpServerBuilder.Create();
 builder.Port(7070)
     .IpAddress(IPAddress.Loopback)
+    .AddGlobalServiceOrConfig(JsonOptions.Instance)
+    .AddGlobalServiceOrConfig<IMessageService>(new SQLiteMessageService("Data Source=Chat.db"))
+    .OnClientConnectedMessageSend((services) =>
+    {
+        var idMessage = MessageFactory.CreateIdentificationMessage();
+        var jsonSerializerOptions = services.Get<JsonSerializerOptions>();
+        return idMessage.ToBytes(jsonSerializerOptions);
+    })
     .Pipeline(pipeline =>
     {
-
+        pipeline.AddComponent(new BytesReadFromStreamComponent())
+            .AddComponent(new ErrorCatchMessageComponent())
+            .AddComponent(new ParseChatMessageComponent())
+            .AddComponent(new AddChatMessageComponent())
+            .AddComponent(new NotifyAllChatComponent());
     });
 
+var tcpServer = builder.Build();
 
-
-TcpServerCore tcpServerCore = new TcpServerCore(IPAddress.Loopback, 7070, new SQLiteMessageService("Data Source=Chat.db"), JsonOptions.Instance);
 ChatHttpServer httpServer = new ChatHttpServer("http://localhost:7071/", JsonOptions.Instance);
 httpServer.AddEndpoints();
 
-Task.WaitAll([ httpServer.StartAsync(), tcpServerCore.RunAsync()]);
+Task.WaitAll([httpServer.StartAsync(), tcpServer.RunAsync()]);
